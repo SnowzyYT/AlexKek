c0cbf2852f5faaf06f595f717522c197c4457ea8
Snowzy <83246567+SnowzyYT@users.noreply.github.com>
2021-11-15 18:25:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,70 +3,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client({disableEveryone: true});
 
 const { token, default_prefix } = require('./config.json');
-
-const fs = require('fs');
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
-
 const config = require('./config.json');
 client.config = config;
-
-const db = require('quick.db');
-
-fs.readdirSync('./commands/').forEach(dir => {
-
-    //in the cmds folder, we gonna check for the category
-    fs.readdir(`./commands/${dir}`, (err, files) => {
-
-        // console log err (catch err)
-        if (err) throw err;
-
-         // checking if the files ends with .js if its a javascript file
-        var jsFiles = files.filter(f => f.split(".").pop() === "js");
-
-         // if there is no cmds in the file it will return
-        if (jsFiles.length <= 0) {
-          console.log("Can't find any commands!");
-          return;
-        }
-
-        
-        jsFiles.forEach(file => {
-
-            // console the loaded cmds 
-            var fileGet = require(`./commands/${dir}/${file}`);
-            console.log(`╠✅》 ${file} was loaded`)
-
-            // gonna let the cmds run
-            try {
-                client.commands.set(fileGet.help.name, fileGet);
-
-                // it search in the cmds folder if there is any aliases
-                fileGet.help.aliases.forEach(alias => {
-                    client.aliases.set(alias, fileGet.help.name);
-              })
-
-            } catch (err) {
-              // catch err in console  
-                return console.log(err);
-            }
-        });
-    });
-});
-
 
 client.on("error", console.error);
 
 client.on('ready', () => {
     console.log('I am ready');
 });
-
-let stats = {
-    serverID: '<ID>',
-    total: "<ID>",
-    member: "<ID>",
-    bots: "<ID>"
-}
 
   client.on('voiceStateUpdate', (oldMember, newMember) => {
   const newUserChannel = newMember.channelID
@@ -92,25 +36,5 @@ let stats = {
     }
   })
   
-client.on("message", async message => {
-
-    if (message.author.bot) return;
-    if (message.channel.type === 'dm') return;
-
-    let prefix;
-// no one did =setprefix
-    let prefixes = await db.fetch(`prefix_${message.guild.id}`);
-    if(prefixes == null) {
-        prefix = "." // this will be the default prefix
-    } else {
-        prefix = prefixes;
-    }
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1)
-    let commands = client.commands.get(cmd.slice(default_prefix.length)) || client.commands.get(client.aliases.get(cmd.slice(default_prefix.length)));
-
-    if(commands) commands.run(client, message, args, prefix);
-  })
 
 client.login(token);
